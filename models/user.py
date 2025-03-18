1c4db18115d335472e6d5a44efa5514ef7777dd0
Keralte _ <christian.nelles@laplateforme.io>
2025-03-18 10:32:05 +0100

    test merge2

--- models/user.py
+++ models/user.py
@@ -3,3 +3,13 @@
 class User:
     def __init__(self):
         pass
+    
+        
+        passw = pwinput.pwinput("mot de passe : ")
+    def connect_db():
+        return mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=passw,
+            database="budget_buddy"
+        )
