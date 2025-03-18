f79df4db496f3a35bde07b6bf5c3abbfd561f7ce
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2025-03-18 10:30:57 +0100

    test merge

--- models/user.py
+++ models/user.py
@@ -1,5 +1,21 @@
 
+import customtkinter as ctk
 
-class User:
-    def __init__(self):
+class User(ctk.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
+    
+        self.label = ctk.CTkLabel(self,text="Connexion", font=("Arial",18))
+        self.label.pack(pady = 10)
+
+        self.button = ctk.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=0, column=0, padx=20, pady=20)
+
+        self.grid_columnconfigure(0,weight=1)
+
+    def button_callback(self):
+        print("button pressed")
+
+
+    def sign_in(self):
         pass
