a2abb1b6bd8f54e767bfe7865b7b0115a2f99de1
Keralte _ <christian.nelles@laplateforme.io>
2025-03-19 15:43:10 +0100

    add transaction

--- models/user.py
+++ models/user.py
@@ -19,14 +19,3 @@ class User(ctk.CTkFrame):
 
     def sign_in(self):
         pass
-
-    
-        
-        passw = pwinput.pwinput("mot de passe : ")
-    def connect_db():
-        return mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password=passw,
-            database="budget_buddy"
-        )
