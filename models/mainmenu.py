33ee5b463e76bab027c465802f42e8d1e35963e5
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2025-03-20 15:04:59 +0100

    add parameter in account_exists

--- models/mainmenu.py
+++ models/mainmenu.py
@@ -31,7 +31,7 @@ class Main_menu(ctk.CTkFrame):
         self.user_email = ctk.CTkLabel(self.user_info_frame, text="Email : lucas@example.com")
         self.user_email.pack()
 
-        self.create_account_button = ctk.CTkButton(self.user_info_frame,text= "Créer un compte",command=self.create_account)
+        self.create_account_button = ctk.CTkButton(self.user_info_frame,text= "Créer un compte",command=self.open_create_account_window)
         self.create_account_button.pack(pady=50)
 
 
@@ -86,25 +86,58 @@ class Main_menu(ctk.CTkFrame):
         finally :
             self.conn.close_db()
 
+
+    def open_create_account_window(self):
+        """open new window to add name account"""
+        self.create_window = ctk.CTkToplevel(self)
+        self.create_window.title("Create an account")
+        self.create_window.geometry("400x200")
+
+        self.account_name_variable = ctk.StringVar(value="Compte courant")
+
+        ctk.CTkLabel(self.create_window, text="Nom du compte :").grid(row=2, column=0, padx=10, pady=5, sticky="n")
+        self.account_name_combobox = ctk.CTkComboBox(self.create_window, values=["Compte courant","Livret A", "Livret Epargne"], variable=self.account_name_variable)
+        self.account_name_combobox.grid(row=2, column=1, padx=10, pady=5)
+        
+        ctk.CTkButton(self.create_window, text="Créer le compte", command=self.create_account).grid(row=3, column=0, columnspan=2, pady=15)
+
+
+    def account_exists(self,account_name,user_id):
+
+        self.conn.cursor.execute("SELECT name FROM accounts WHERE name = %s AND user_id = %s" , 
+                                (account_name,user_id))
+        account = self.conn.cursor.fetchone()
+
+        return account is not None 
+    
+
     def create_account(self):
         """create an account"""
         
         self.conn.connect_db()
+        user_id = self.conn.get_user_id()
+        account_name = self.account_name_variable.get()
 
+        print(f"[creat_account] {account_name}")
+
+        if self.account_exists(account_name,user_id):
+            messagebox.showinfo("Erreur", "Le compte banquaire existe déjà")
+            self.create_window.destroy()
+            self.open_create_account_window()
+            return
         try:
-
-            user_id = self.conn.get_user_id()
-            
-            querry = """INSERT INTO accounts (balance,user_id)
-                        VALUES (%s,%s)
+                        
+            querry = """INSERT INTO accounts (name,balance,user_id)
+                        VALUES (%s,%s,%s)
                     """
-            values = (0,user_id)
+            values = (account_name,0,user_id)
 
             self.conn.cursor.execute(querry,values)
             self.conn.mydb.commit()
 
             messagebox.showinfo("Création de compte", "Création de compte effectué")
-            
+            self.select_account()
+            self.create_window.destroy()            
 
         except Exception as e:
             print("Erreur lors de la connexion :", e)
@@ -123,30 +156,31 @@ class Main_menu(ctk.CTkFrame):
             user_id = self.conn.get_user_id() 
             print(f"🔍 [Main_menu][select_account] user_id récupéré : {user_id}")  
 
-            querry = """SELECT id,balance
+            querry = """SELECT id,name,balance
                         FROM accounts
                         WHERE user_id = %s
             """
 
             self.conn.cursor.execute(querry,(user_id,))
             data_accounts = self.conn.cursor.fetchall()
-            print(f"data account {data_accounts}")
 
-            for radiobutton in self.radiobuttons_accounts:
+
+            for radiobutton in self.radiobuttons_accounts[:]:  
                 radiobutton.destroy()
-                self.radiobuttons_accounts.clear()
+
+            self.radiobuttons_accounts.clear() 
+
 
             self.variable.set("") 
 
-
             for i, account in enumerate(data_accounts):
-                account_text = f"Account N° : {account[0]} - Solde : {account[1]}€"
+                account_text = f"Account N° : {account[0]} - Name : {account[1]} - Solde : {account[2]}€"
                 radiobutton = ctk.CTkRadioButton(
                     self.user_info_frame, 
                     text=account_text,
                     variable=self.variable,
-                    value=[account[0]])
-                radiobutton.pack(pady=5)
+                    value=account[0])
+                radiobutton.pack(pady=3, padx=50,anchor="w", fill="x")
                 self.radiobuttons_accounts.append(radiobutton) 
 
         except Exception as e:
